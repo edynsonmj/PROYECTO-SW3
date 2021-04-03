11079fcf0211948e31a1772e53ee0555c4e6369d
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2021-04-03 17:18:51 -0500

    estilos de la pagina registro

--- application/views/vistas_crud/insertar.php
+++ application/views/vistas_crud/insertar.php
@@ -7,21 +7,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <?php $this->load->view('partes/header'); ?>
 
 <body>
+	<div class=contenForm>
+		<form class="datosForm" name="datos" method="POST" action="<?php echo base_url(); ?>index.php/datos/insertUsuario">
 
-	<form id="datos" name="datos" method="POST" action="<?php echo base_url(); ?>index.php/datos/insertUsuario">
-
-		Nombre de usuario <input name="login" type="text" minlength="2" /><br />
-		password <input name="password" type="text" minlength="5" /><br />
-		Role <select name="rol">
-			<option value="administrador">Administrador</option>
-			<option value="jefe">Jefe</option>
-			<option value="cliente" selected>Cliente</option>
-		</select>
-		<br/>
-		Correo electronico <input name="email" type="email" /><br />
-		<input type="submit" value="registrar" />
-	</form>
-
+			<label class=labelForm>Login</label> <br><input class=campoForm name="login" type="text" minlength="2" /><br><br>
+			<label class=labelForm>Password</label><br> <input class=campoForm name="password" type="text" minlength="5" /><br /><br>
+			<label class=labelForm>Role</label><br> <select class=campoForm name="rol">
+				<option value="administrador">Administrador</option>
+				<option value="jefe">Jefe</option>
+				<option value="cliente" selected>Cliente</option>
+			</select>
+			<br /><br>
+			<label class=labelForm>Email</label><br> <input class=campoForm name="email" type="email" /><br /><br>
+			<input class=boton type="submit" value="registrar" /><br>
+		</form>
+	</div>
 </body>
 <?php $this->load->view('partes/foot'); ?>
+
 </html>
